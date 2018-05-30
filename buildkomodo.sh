5d59264450787f80048b37d49488b11997a1d293
blackjok3r <blackjok3r@gmail.com>
2018-05-30 13:31:04 +0200

    modified:   buildkomodo.sh

--- buildkomodo.sh
+++ buildkomodo.sh
@@ -3,10 +3,9 @@ sudo apt-get update
 sudo apt-get install build-essential pkg-config libc6-dev m4 g++-multilib autoconf libcurl4-gnutls-dev libtool ncurses-dev unzip git python python-zmq zlib1g-dev wget libcurl3-gnutls-dev bsdmainutils automake curl
 #Install Komodo
 cd ~
-git clone https://github.com/jl777/komodo
+git clone https://github.com/libscott/komodo.git -b momo
 cd komodo
-git checkout jl777
 ./zcutil/fetch-params.sh
 ./zcutil/build.sh -j$(nproc)
-sudo ln -sf /home/$USER/komodo/src/komodo-cli /usr/local/bin/komodo-cli
-sudo ln -sf /home/$USER/komodo/src/komodod /usr/local/bin/komodod
+#sudo ln -sf /home/$USER/komodo/src/komodo-cli /usr/local/bin/komodo-cli
+#sudo ln -sf /home/$USER/komodo/src/komodod /usr/local/bin/komodod
